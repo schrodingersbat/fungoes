df498eae5df30c7d78e5664d69baee343f895d0f
svf <svf@qloot.co>
2014-04-07 14:07:45 -0700

    removed commeted out line

--- qry_SimplePitches.sql
+++ qry_SimplePitches.sql
@@ -10,7 +10,6 @@ pitches.type,
 pitches.des,
 atbats.event
 FROM pitches
-#INNER JOIN pitches ON atbats.ab_id = pitches.ab_id
 INNER JOIN pitch_types ON pitches.pitch_type = pitch_types.id
 INNER JOIN atbats ON pitches.ab_id = atbats.ab_id
 INNER JOIN players ON atbats.batter = players.eliasid
