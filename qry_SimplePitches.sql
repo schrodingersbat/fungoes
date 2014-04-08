5221f4f6f83ffaa3c7f79a7b1b5db6b66997a82f
svf <svf@qloot.co>
2014-04-08 07:44:53 -0700

    added game id

--- qry_SimplePitches.sql
+++ qry_SimplePitches.sql
@@ -3,6 +3,7 @@ This is a simple query that joins on pitch_types, atbats and players to reveal
 non-coordinate Pitch f/x data on all Jeff Samardzija pitches.
 ***/
 select 
+games.game_id,
 pitches.pitch_id,
 pitches.ab_id,
 CONCAT(players.first, " ", players.last) as batter,
@@ -18,8 +19,10 @@ pitches.des,
 atbats.event,
 atbats.hit_type
 FROM pitches
-right JOIN pitch_types ON pitches.pitch_type = pitch_types.id
-right JOIN atbats ON pitches.ab_id = atbats.ab_id
-right JOIN players ON atbats.batter = players.eliasid
+#INNER JOIN pitches ON atbats.ab_id = pitches.ab_id
+INNER JOIN pitch_types ON pitches.pitch_type = pitch_types.id
+INNER JOIN atbats ON pitches.ab_id = atbats.ab_id
+INNER JOIN players ON atbats.batter = players.eliasid
+INNER JOIN games ON atbats.game_id = games.game_id
 WHERE pitcher = 502188 #Jeff Samardzija
 
